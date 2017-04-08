db73261127ad336abe217d164aefd5d2be6fcc70
Tobias Ford <toby.ford@pobox.com>
2017-04-08 07:24:00 -0400

    just got the idea of doing cheatsheets from http://ricostacruz.com/cheatsheets/

---
+++ content/cheatsheets/devise.d
@@ -0,0 +1,14 @@
++++
+featured = ""
+date = "2017-04-08T07:09:30-04:00"
+author = ""
+featuredpath = ""
+featuredalt = ""
+title = "devise"
+linktitle = ""
+description = ""
+categories = []
+draft = true
+
++++
+
